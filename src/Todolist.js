7d6ab0a6389a0a1c2852c92a709c7d3af701d93a
Rebornjiang <626104624@qq.com>
2020-07-14 17:25:47 +0800

    初始化

---
+++ src/Todolist.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react'
+
+class Todolist extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className="todolist-wrap">
+          Hello world
+        </div>
+      </Fragment >
+    )
+  }
+}
+
+export default Todolist
+
